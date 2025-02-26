a6b20da680902c90afd826e3d61b140883f93802
andredw13L <andreribeiro13l@gmail.com>
2025-02-26 17:57:05 -0300

    Practice: Styling Tex

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,19 @@ void main() {
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: <Color> [
-                Colors.deepPurple,
-                Colors.purpleAccent
-              ], 
+              colors: <Color>[Colors.deepPurple, Colors.purpleAccent],
               begin: Alignment.topLeft,
-              end: Alignment.bottomRight
+              end: Alignment.bottomRight,
             ),
           ),
           child: const Center(
-            child: Text('Hello World!'),
+            child: Text(
+              'Hello World!',
+              style: TextStyle(fontSize: 28, color: Colors.white),
+            ),
           ),
         ),
-      )
-    )
+      ),
+    ),
   );
 }
-
